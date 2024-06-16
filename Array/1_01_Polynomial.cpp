d47c6b694f08d758251bf9bf7db4348600da1f48
Benny0w0Liu <bennyliu9005@gmail.com>
2024-06-17 00:55:07 +0800

    pol

--- Array/1_01_Polynomial.cpp
+++ Array/1_01_Polynomial.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int Polynomial[5] = {12	,7	,9	,13	,3};
+	int Polynomial[5] = {12, 7, 9, 13, 3};
 	//print Polynomial
     for (int i = 5 - 1; i > -1; i--){
         cout << Polynomial[i] << "x^" << i;
--- Array/1_02_Polynomial.cpp
+++ Array/1_02_Polynomial.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 int main(){
     int Polynomial[2][2]= {{4,3},{0,12}};
     for (int i = 0; i < 2; i++){
