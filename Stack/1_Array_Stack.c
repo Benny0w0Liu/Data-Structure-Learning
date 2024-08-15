2bf8ca40e893f402eba64095c1a4400dfafdb431
Benny0w0Liu <bennyliu9005@gmail.com>
2024-08-15 23:56:05 +0800

    Add array stack

--- Stack/1_Array_Stack.c
+++ Stack/1_Array_Stack.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
-#include<stdlib.h>
+void push(int stack[],int* top,int size,int n) {
+	if(*top==size-1) return;
+	stack[++(*top)]=n;
+}
+void pop(int* stack,int* top){
+	if(*top==-1) return;
+	(*top)--;
+}
 int main(){
-	int A[10];
+	int stack[10],size=10 ,top=-1,i;
+	push(stack, &top, size,3);
+	push(stack, &top, size,0);
+	push(stack, &top, size,7);
+	push(stack, &top, size,5);
+	pop(stack, &top);
+	for(i=top;i>=0;i--)
+		printf("%d : %d\n",i, stack[i]);
+	return 0;
 } 
